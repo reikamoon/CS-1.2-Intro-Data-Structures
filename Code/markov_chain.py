0bb5630eb5bb6b75aaaf062eb6f59a5c16701e8a
Anjelica <ambrosio.anjelica@gmail.com>
2019-11-18 11:52:09 -0800

    updated markov

--- Code/markov_chain.py
+++ Code/markov_chain.py
@@ -1,34 +1,39 @@
 from dictogram import Dictogram
 from random import choice
 
-def markov_chain(word_list):
-    markov = {}
-    for i in range(len(word_list)):
-        if word_list[i] not in markov:
-            markov[word_list[i]] = []
-        if i < len(word_list) - 1:
-            markov[word_list[i]].append(word_list[i + 1])
+class Markov():
+    def __init__(self, word_file):
+        self.word_list = read_file(word_file)
+        self.markov = self.make_chain(self.word_list)
 
-    for key in markov:
-        markov[key] = Dictogram(markov[key])
+    def make_chain(self, word_list):
+        markov = {}
+        for i in range(len(word_list)):
+            if word_list[i] not in markov:
+                markov[word_list[i]] = []
+            if i < len(word_list) -1:
+                markov[word_list[i]].append(word_list[i + 1])
 
-    return markov
+        for key in markov:
+            markov[key] = Dictogram(markov[key])
 
-def random_walk(chain, length):
-    output = []
-    output.append(choice(tuple(chain.keys())))
-    for i in range (length):
-        output.append(chain[output[i]].sample())
+            return markov
 
-    string = ""
-    for word in output:
-        string += word + " "
+    def walk(self, length):
+        output = []
+        output.append(choice(tuple(self.markov.keys())))
+        for i in range(length):
+            output.append(self.markov[output[i]].sample())
 
-    return string
+        string = ""
+        for word in output:
+            string += word + " "
+        return string
 
-def sentence():
-    text = 'one fish two fish red fish blue fish'
-    print(random_walk(markov_chain(text),10))
+    def main(num):
+        markov = Markov('markov.txt')
+
+        return markov.walk(num)
 
 if __name__ == '__main__':
-    sentence()
+    main(5)
